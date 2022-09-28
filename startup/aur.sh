8afdeecbf3df198c950732d2c7d4d96037309b1d
gman72 <tonygates2@gmail.com>
2022-09-28 13:42:03 -0500

    Added new items to install scripts

--- startup/aur.sh
+++ startup/aur.sh
@@ -4,5 +4,5 @@ git clone https://aur.archlinux.org/pikaur.git
 chown -r gman72:root pikaur/
 cd pikaur
 sudo -u gman72 makepkg -fsri --noconfirm
-sudo -u gman72 pikaur -S spotify-tui teams genymotion networkmanager-dmenu-git openrazer-meta polychromatic universal-android-debloater ttf-dejavu-sans-code ttf-material-design-icons zscroll-git --noconfirm --needed
+sudo -u gman72 pikaur -S ncspot spotify-tui teams genymotion networkmanager-dmenu-git openrazer-meta polychromatic universal-android-debloater ttf-dejavu-sans-code ttf-material-design-icons zscroll-git --noconfirm --needed
 	
