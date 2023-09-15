7cc444184ec8f8cae8c86d2dfe5993412f6e596a
tony gates <tonygates2@gmail.com>
2023-09-15 12:23:50 -0500

    new changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,3 +8,11 @@ if status is-login
         exec startx -- -keeptty
     end
 end
+set -x PATH $PATH $HOME/flutter/flutter/bin
+set -x PATH $PATH $HOME/flutter/flutter/.pub-cache/bin
+set -x ANDROID_SDK_ROOT /opt/android-sdk
+set -x PATH $PATH $ANDROID_SDK_ROOT/tools/bin $ANDROID_SDK_ROOT/platform-tools/
+set -x JAVA_HOME /usr/lib/jvm/default
+set -xg CHROME_EXECUTABLE /usr/bin/google-chrome-stable
+
+
